5820431520d4c87e5c371f907c4d9a96b7b49cf1
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-05-31 00:50:59 +0800

    Update exercise6.c
    //documentation section 
    /* Exercise 6 - Binary Search*/
    
    //pre-processor section 
    #include<stdio.h>
    
    //global variable section 
    
    
    //main function section 
    int main(){
        //write here your program
        int n,a,i,count=0;
        printf("Enter the number of elements in the array:\n");
        scanf("%d",&n);
        int array[n];
        printf("Enter the numbers in the array:\n");
        for(i=0;i<n;i++)
        scanf("%d",&array[i]);
        printf("Enter the number to be searched: ");
        scanf("%d",&a);
    
        for(i=0;i<n;i++)
        {
            if(array[i]==a)
            {
                printf("Element with value %d is present in arr[]\n",a,i+1);
                count++;
            }
        }
        if(count==0)
        printf("Element with value %d is not present in arr[]\n",a);
        else
        printf("Element with value %d is present %d times in array[]",a,count);
        return 0;
        
    
    }
    
    //user-defined section

--- exercise6.c
+++ exercise6.c
@@ -10,8 +10,31 @@
 //main function section 
 int main(){
     //write here your program
+    int n,a,i,count=0;
+    printf("Enter the number of elements in the array:\n");
+    scanf("%d",&n);
+    int array[n];
+    printf("Enter the numbers in the array:\n");
+    for(i=0;i<n;i++)
+    scanf("%d",&array[i]);
+    printf("Enter the number to be searched: ");
+    scanf("%d",&a);
+
+    for(i=0;i<n;i++)
+    {
+        if(array[i]==a)
+        {
+            printf("Element with value %d is present in arr[]\n",a,i+1);
+            count++;
+        }
+    }
+    if(count==0)
+    printf("Element with value %d is not present in arr[]\n",a);
+    else
+    printf("Element with value %d is present %d times in array[]",a,count);
+    return 0;
     
 
 }
 
-//user-defined section 
+//user-defined section
